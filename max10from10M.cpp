6c30cb7d7980dcf03201fee14611ce7ad468c49d
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-03 16:46:17 +0530

    Update max10from10M.cpp

--- max10from10M.cpp
+++ max10from10M.cpp
@@ -3,6 +3,19 @@
 //CASE1: If the numbers range from 0 to say 1000 (or so).In this case we can apply Counting Sort algoritm.
 vector<int> max10(vector<int> arr)
 {
+  vector<int> ans;
   int c[1000];
-  for (int i=0; i< arr.size(); i++
+  for (int i=0; i< arr.size(); i++){
+    c[arr[i]]++;
+  }
+  int count=0;
+  for (int i=1000;i>=0;i--){
+    count+=c[i];
+     for (int j=0; j<count%10; j++)
+      {
+        ans.push_back(i);
+      }
+    if (count>=10) break;
+  }
+  return ans;
 }
