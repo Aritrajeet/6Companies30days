9e8a6eb645bf2bebd3289f830774f5068b48f495
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-05 11:09:59 +0530

    GS

---
+++ uglyNumber.cpp
@@ -0,0 +1,14 @@
+	ull getNthUglyNo(int n) {
+	   set <long long int> s;
+	   s.insert(1);
+	   n--;    //since we included 1 as the ugly number
+	   while(n--){
+	        auto it = s.begin();
+	        long long int x = *it;
+	        s.erase(it);
+	        s.insert(x*2);
+	        s.insert(x*3);
+	        s.insert(x*5);
+	    } 
+        return *s.begin();
+	}
