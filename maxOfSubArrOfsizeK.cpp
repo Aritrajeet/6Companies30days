cb0751530c60c2cf094f174b3ad2d866a628b91c
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-06 11:45:06 +0530

    AM

---
+++ maxOfSubArrOfsizeK.cpp
@@ -0,0 +1,21 @@
+//Using STL Multiset(for handling duplicates) as sliding window.   
+vector <int> max_of_subarrays(int *arr, int n, int k)
+    {        
+        multiset<int> s;
+        vector<int>ans;
+        int maxi = INT_MIN;
+        for (int i=0; i<k; i++)
+        {
+            maxi = max(maxi,arr[i]);
+            s.insert(arr[i]);
+        }
+        int left = 0;
+        int right = k;
+        ans.push_back(maxi);
+        while(right<n){
+            s.erase(arr[left++]);
+            s.insert(arr[right++]);
+            ans.push_back(*s.rbegin());
+        }
+        return ans;
+    }
