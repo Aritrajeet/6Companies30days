62174635614787ac44de86f2412aec098b22370e
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-11 11:32:18 +0530

    AM

---
+++ Amazon/countWaysToNthStair.cpp
@@ -0,0 +1,4 @@
+ //We just need to count the number of possible 2s. It will give all the possible combinations. And add 1 for the subset which is made of all 1s.
+long long countWays(int m){
+      return (long long)m/2 +1;
+  }
