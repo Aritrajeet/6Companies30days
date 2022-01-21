8cd65b1e72519f0045374789ab7440f187c56f43
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-22 00:08:17 +0530

    Create minSumPartition.cpp

---
+++ Microsoft/minSumPartition.cpp
@@ -0,0 +1,31 @@
+int minDifference(int arr[], int n)  { 
+    // Your code goes here
+    int sum = 0;
+    for(int i=0;i<n;i++)
+        sum += arr[i];
+    
+    bool dp[n+1][sum+1];
+    for(int i=0;i<=n;i++)
+    for(int j=0;j<=sum;j++)
+        {
+            if(j == 0)
+                dp[i][j] = true;
+            else if(i == 0)
+                dp[i][j] = false;
+            else if(arr[i-1] > j)
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+        }
+  
+    
+    int diff = INT_MAX;
+    for(int i=0;i<=sum/2;i++)
+    {
+        int first = i;
+        int second = sum-i;
+        if(dp[n][i] == true and diff > abs(first-second))
+            diff = abs(first-second);
+    }
+    return diff;
+} 
