fa6044a1297a57a0d418c5f5aa4c62222ad5e7cb
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-01-31 22:31:23 +0530

    Update pathWithMaxProb.cpp

--- Walmart/pathWithMaxProb.cpp
+++ Walmart/pathWithMaxProb.cpp
@@ -1,21 +1,21 @@
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-    vector<vector<pair<int, double>>> to(n);
+    vector<vector<pair<int, double>>> v(n);
         for(int i=0;i<edges.size();i++){
-            to[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
-            to[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
+            v[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
+            v[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
         }
-        vector<double> Prob(n, 0);
-        queue<int> Q;
-        Q.push(start);
-        Prob[start] = 1;
-        while (!Q.empty()){
-            int node = Q.front();
-            Q.pop();
-            for (auto child : to[node]){
-                int nxt = child.first;
-                double p = child.second;
-                if (Prob[nxt] < Prob[node] * p) Prob[nxt] = Prob[node] * p, Q.push(nxt);
+        vector<double> dp(n, 0);
+        queue<int> q;
+        q.push(start);
+        dp[start] = 1;
+        while (!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for (auto child : v[curr]){
+                double f = child.first;
+                double s = child.second;
+                if (dp[nxt] < dp[node] * s) dp[f] = dp[f] * p, q.push(f);
             }
         }
-        return Prob[end];
+        return dp[end];
     }
